e0e2052ff13bb3dcbb5daa728d40fd1b58a8f3ce
Naj <najneen.pendhari@gmail.com>
2020-08-27 10:13:12 +0530

    create a new repository -- Naj

---
+++ registration.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
+
+
+@app.route("/registration")
+def welcome_page():
+    return render_template('userinfo.html')
+
+@app.route("/save/",methods=["POST"])
+def save_user_info():
+    print('METHOD TYPE', request.method)
+    print('GET METHOD TYPE', request.args)  #
+    print('POST METHOD TYPE', request.form)  # post
+    firstname = request.form['fname']
+    lastname =  request.form['lname']
+    gender = request.form['gender']
+    address = request.form['address']
+    skills = request.form.getlist['skills']
+    courses = request.form.getlist['courses']
+    print('--------------------------------')
+    print('FirstName : ', firstname)
+    print('Lastname : ', lastname)
+    print('Gender : ', gender)
+    print('Address : ', address)
+    print('Skills : ', skills)
+    print('Courses : ', courses)
+    print('--------------------------------')
+    return render_template('userinfo.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=1828)
